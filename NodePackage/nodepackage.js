c9fc955ebce59bea630771979ba748365053ac11
artoD2 <artosekepyan@gmail.com>
2020-09-28 22:32:35 -0600

    added nodePackage assignment

--- NodePackage/nodepackage.js
+++ NodePackage/nodepackage.js
@@ -1,13 +1,19 @@
 function hello() {
-    console.log("Hello World")
+    var Color = require('color');
+    var color = Color.rgb(255, 255, 255)
+    console.log(color("Hello World"))
 }
 hello()
 
 // stretch your thoughts questions
 
-/* Node.js is anopen source platform for 
+/* Question 1
+Node.js is anopen source platform for 
 testing code outside of a web browser. As for npm 
 it's a package manager for installing and 
 managing dependenies while you work. However in 
 to use npm you must first have node installed. */
-/* */
+
+/* Question 2
+Allow your node_modules to be inside 
+your project directory. */
